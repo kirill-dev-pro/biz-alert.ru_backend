59db3ea7595731c4d7f28f20dcc75301ddec7157
Кирилл <opoxmeloid@yandex.ru>
2020-04-14 22:17:45 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ RUN npm install
 COPY . .
 
 EXPOSE 8080
-CMD [ "node", "server.js" ]
+CMD [ "node", "index.js" ]
--- models/Point.js
+++ models/Point.js
@@ -1,4 +1,4 @@
-import { Schema } from 'mongoose'
+const { Schema } = require('mongoose')
 
 const PointSchema = new Schema({
   address: String,
